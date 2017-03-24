f74e6d1c3de906a91389e819525a3a59b029b7f2
yuskam <ybkamaleri@gmail.com>
2017-03-24 16:02:35 +0100

    filter

--- codes/fig01.R
+++ codes/fig01.R
@@ -6,9 +6,30 @@ data01 <- read.csv("indikator01.csv", sep = ",", header = TRUE)
 data02 <- read.csv("indikator02.csv", sep = ",", header = TRUE)
 data03 <- read.csv("indikator03.csv", sep = ",", header = TRUE)
 
+data01$n1 <- 30
+data01$n2 <- 60
+data01$n3 <- 70
+
+data02$n1 <- 45
+data02$n2 <- 75
+data02$n3 <- 95
+
+data03$n1 <- 60
+data03$n2 <- 100
+data03$n3 <- 105
+
+
 data01$ind <- 1
 data02$ind <- 2
 data03$ind <- 3
+
+dataset <- list(data01, data02, data03) #do.call require a list
+
+dataall <- do.call(rbind, dataset) #combine all the data.frames
+
+dataall <- readRDS("dataall.RDS")
+
+
 
 ## Subsetting
 library('dplyr')
@@ -39,7 +60,7 @@ formattable(data02, list(
 
 
 
-data01[with(data01, sykehus %in% valgsyk, year %in% yearfilter),]
+dataall[with(dataall, sykehus %in% valgsyk, year %in% yearfilter),]
 
 
 
@@ -61,3 +82,28 @@ data <- data.frame(data01)
 
 
 ## TEST
+
+valgdata <- function(data, aar = NULL, sykehus = NULL, region = NULL){
+
+    "%i%" <- intersect
+    data <- data
+    yearall <- unique(data$year)
+    sykall <- unique(data$sykehus)
+    regall <- unique(data$region)
+
+    yearfil <- if (is.null(aar)) {which(data$year %in% yearall)
+               } else {yearfil <- which(data$year %in% aar)}
+
+    sykfil <- if (is.null(sykehus)) {which(data$sykehus %in% sykall)
+              } else {sykfil <- which(data$sykehus %in% sykehus)}
+
+    regfil <- if (is.null(region)) {which(data$region %in% regall)
+              } else {regfil <- which(data$region %in% region)}
+
+
+    indFil <- yearfil %i% sykfil %i% regfil
+
+    data[indFil,]
+}
+
+valgdata(data = dataall, sykehus = c("Oslo", "Drammen", "Tromsø", "Nasjonal"), aar = c(2015, 2016))
